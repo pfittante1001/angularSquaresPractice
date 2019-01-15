a222ad9b59059c6646e672802cfd6937b0af8e14
pfittante1001 <pfittante@yahoo.com>
2019-01-15 18:52:55 +0000

    finished back end kinda

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 import './styles.css';
-console.log('Ready to Party');
+import { getRandomInt, ready } from './utils';
+
+let secretNumber;
+
+function doIt() {
+    const squares = document.querySelectorAll('.square');
+    let squareNumber = 1;
+    squares.forEach(function (square) {
+        square.addEventListener('click', handleClick);//adds the  onClick event handler to each square
+        square.dataset.number = squareNumber.toString();
+        squareNumber++;
+    });
+
+    //gets a random number from funtion in utils.js
+    secretNumber = getRandomInt(1,6);
+    console.log('secret is', secretNumber);
+}
+
+function handleClick(evt){
+    if(parseInt(this.dataset.number) === secretNumber) {
+        console.log('You Win!');
+    }
+    
+}
+ready(doIt);
